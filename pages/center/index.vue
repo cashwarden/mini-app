a124d451128f29b11e5789985c07afda6c886c09
forecho <caizhenghai@gmail.com>
2021-03-19 14:41:49 +0800

    fix: 优化会员页面

--- pages/center/index.vue
+++ pages/center/index.vue
@@ -1,5 +1,6 @@
 <template>
   <view>
+    <u-toast ref="uToast" />
     <view class="u-flex user-box u-p-l-30 u-p-r-20 u-p-b-30">
       <view class="u-m-r-10">
         <u-avatar :src="user.avatar" size="120"></u-avatar>
@@ -7,7 +8,7 @@
       <view class="u-flex-1">
         <view class="u-font-18 u-p-b-20">{{ user.username }}</view>
         <view class="u-font-14 u-tips-color" v-if="user.pro">
-          Pro 会员有效期：{{ user.pro.ended_at }}
+          Pro 会员有效期：{{ user.pro.ended_at | date("yyyy-mm-dd") }}
         </view>
         <view class="u-font-14 u-tips-color" v-if="!user.pro">
           Free 会员有效期：永久
@@ -20,22 +21,26 @@
 
     <view class="u-m-t-20">
       <u-cell-group>
-        <u-cell-item icon="rmb-circle" title="升级会员"></u-cell-item>
+        <u-cell-item
+          icon="rmb-circle"
+          @tap="showToast"
+          title="升级会员"
+        ></u-cell-item>
       </u-cell-group>
     </view>
 
     <view class="u-m-t-20">
       <u-cell-group>
-        <u-cell-item icon="star" title="账户"></u-cell-item>
-        <u-cell-item icon="photo" title="账本"></u-cell-item>
-        <u-cell-item icon="coupon" title="定时"></u-cell-item>
-        <u-cell-item icon="heart" title="规则"></u-cell-item>
+        <u-cell-item icon="star" @tap="showToast" title="账户"></u-cell-item>
+        <u-cell-item icon="photo" @tap="showToast" title="账本"></u-cell-item>
+        <u-cell-item icon="coupon" @tap="showToast" title="定时"></u-cell-item>
+        <u-cell-item icon="heart" @tap="showToast" title="规则"></u-cell-item>
       </u-cell-group>
     </view>
 
     <view class="u-m-t-20">
       <u-cell-group>
-        <u-cell-item icon="setting" title="设置"></u-cell-item>
+        <u-cell-item icon="setting" @tap="showToast" title="设置"></u-cell-item>
       </u-cell-group>
     </view>
   </view>
@@ -63,6 +68,12 @@ export default {
           .catch((e) => {
             resolve([]);
           });
+      });
+    },
+    showToast() {
+      this.$refs.uToast.show({
+        title: "暂未开发",
+        type: "warning",
       });
     },
   },
